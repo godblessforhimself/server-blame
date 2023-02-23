2a995a067c91e7117fe57c8edc013c8811d8c344
Jin Tao <godblessforhimself@users.noreply.github.com>
2023-02-23 18:57:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-BASEDIR=$(dirname "$0")
-task_list=$(crontab -l)
+BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
+task_list="$(crontab -l)"
 task="* * * * * bash ${BASEDIR}/measure-load.sh"
-if echo "${task_list}" | grep ${task}; then
-    echo "${task} exist";
+if (echo "${task_list}" | grep "${task}" 1>/dev/null); then
+    echo "exist"
 else
-    echo "$task_list" && echo $task | crontab -
+    (echo "${task_list}" && echo "${task}") | crontab -
 fi
